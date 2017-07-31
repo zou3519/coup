8c5d529598e74b9588a7420890c3404444618699
richard <zou3519@gmail.com>
2017-07-31 16:25:20 -0400

    determinize partial game state

--- src/main/scala/coup/core/CoupPartialGameState.scala
+++ src/main/scala/coup/core/CoupPartialGameState.scala
@@ -1,6 +1,8 @@
 package coup.core
 
 import scala.collection.immutable.Seq
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 class CoupPartialGameState(
     val courtDeckSize: Int,
@@ -13,5 +15,30 @@ class CoupPartialGameState(
     val me: PlayerT) {
 
   /* Generates a random CoupGameStage based on this partial state */
-  def determinize: CoupGameState = ???
+  def determinize: CoupGameState = {
+
+    // Figure out what cards are not shown
+    val hiddenCards = Character.characters.flatMap(x => ArrayBuffer(x, x, x)).to[ArrayBuffer]
+    hiddenCards --= myInfluences
+    discardPile.foreach(hiddenCards --= _)
+
+    // Split hidden cards among the deck and the opponent's hand
+    val courtDeck = new CourtDeck(hiddenCards)
+    val opponentsInfluence = (1 to opponentInfluencesSize).map(_ => courtDeck.takeRandomCard())
+
+    // Construct influences
+    val influences = me match {
+      case 0 => ArrayBuffer(myInfluences, opponentsInfluence).map(_.to[ArrayBuffer])
+      case 1 => ArrayBuffer(opponentsInfluence, myInfluences).map(_.to[ArrayBuffer])
+    }
+
+    new CoupGameState(
+      courtDeck,    // deck
+      discardPile.map(_.to[ArrayBuffer]).to[ArrayBuffer],  // discardPile
+      coins.to[ArrayBuffer],        // coins
+      influences,   // influences
+      currentPlay.to[ArrayBuffer],  // current play
+      pendingStages.to[mutable.Queue] // pending stages
+    )
+  }
 }
